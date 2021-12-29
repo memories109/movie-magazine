efd94cc92c9ac9db4793d62cc17136b3f043166a
Ghosts <memories109@gmail.com>
2021-12-29 15:02:20 +0900

    trending

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,15 +1,26 @@
 import React, {  useEffect, useState } from 'react';
 import axios  from 'axios';
-import { Grid, GridItem } from '@chakra-ui/react'
+import { Grid, GridItem, Box, Image,Container,  Badge } from '@chakra-ui/react'
+import { StarIcon } from '@chakra-ui/icons'
 
 export default function Trending() {
 
     let [hotList, setHotList] = useState([]);
+    const property = {
+      imageUrl: 'https://bit.ly/2Z4KKcF',
+      imageAlt: 'Rear view of modern home with pool',
+      beds: 3,
+      baths: 2,
+      title: 'Modern home in city center in the heart of historic Los Angeles',
+      formattedPrice: '$1,900.00',
+      reviewCount: 34,
+      rating: 4,
+    }
 
     useEffect( ()=> {
         axios.get('https://api.themoviedb.org/3/trending/all/day?api_key=aea9844cb6188c7686a5bdb55f70eb2a&page=1')
         .then( (result)=> {
-          console.log(result);
+          setHotList(result.results);
         })
         .catch(function (error) {
           if (error.response) {
@@ -33,14 +44,66 @@ export default function Trending() {
     }) ;
 
   return (
-    <div>
-       <Grid templateColumns='repeat(5, 1fr)' gap={6}>
-        <GridItem w='100%' h='10' bg='blue.500' />
-        <GridItem w='100%' h='10' bg='blue.500' />
-        <GridItem w='100%' h='10' bg='blue.500' />
-        <GridItem w='100%' h='10' bg='blue.500' />
-        <GridItem w='100%' h='10' bg='blue.500' />
-      </Grid>
+    <div> 
+      <Box boxSize='sm'>
+        <Image src='https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg' alt='Dan Abramov' />
+      </Box>
+      <Container>
+        {hotList.overview}
+      </Container>
+        
+      <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+        <Image src={property.imageUrl} alt={property.imageAlt} />
+
+        <Box p='6'>
+          <Box display='flex' alignItems='baseline'>
+            <Badge borderRadius='full' px='2' colorScheme='teal'>
+              New
+            </Badge>
+            <Box
+              color='gray.500'
+              fontWeight='semibold'
+              letterSpacing='wide'
+              fontSize='xs'
+              textTransform='uppercase'
+              ml='2'
+            >
+              {property.beds} beds &bull; {property.baths} baths
+            </Box>
+          </Box>
+
+          <Box
+            mt='1'
+            fontWeight='semibold'
+            as='h4'
+            lineHeight='tight'
+            isTruncated
+          >
+            {property.title}
+          </Box>
+
+          <Box>
+            {property.formattedPrice}
+            <Box as='span' color='gray.600' fontSize='sm'>
+              / wk
+            </Box>
+          </Box>
+
+          <Box display='flex' mt='2' alignItems='center'>
+            {Array(5)
+              .fill('')
+              .map((_, i) => (
+                <StarIcon
+                  key={i}
+                  color={i < property.rating ? 'teal.500' : 'gray.300'}
+                />
+              ))}
+            <Box as='span' ml='2' color='gray.600' fontSize='sm'>
+              {property.reviewCount} reviews
+            </Box>
+          </Box>
+        </Box>
+      </Box>
     </div>
   )
 }
