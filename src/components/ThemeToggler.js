717a5ec769ccc91ab1e1d8b7df8988580a0f0788
Ghosts <memories109@gmail.com>
2021-12-30 08:41:49 +0900

    themetoggler icon

--- src/components/ThemeToggler.js
+++ src/components/ThemeToggler.js
@@ -1,11 +1,11 @@
 import { IconButton, Box, useColorMode } from '@chakra-ui/react'
-
+import { MoonIcon, SunIcon } from '@chakra-ui/icons'
 export default function ThemeToggler() {
     const { colorMode, toggleColorMode } = useColorMode();
     return (
       <Box textAlign="right" py={4} mr={12}>
         <IconButton
-          icon={colorMode === 'light' ? 'MoonIcon' : 'SunIcon'}
+          icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
           onClick={toggleColorMode}
           variant="ghost"
         />
